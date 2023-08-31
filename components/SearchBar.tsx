3ed682b70dcd06e4bf6adb77687d2bf3a4b156df
jurecapuder <capuder.j@gmail.com>
2023-09-01 01:10:40 +0200

    Creating search bar

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,8 +1,22 @@
-import React from 'react'
+"use client";
+
+import { useState } from "react";
+
+import { SearchManufacturer } from "./";
 
 const SearchBar = () => {
+  const [manufacturer, setManufacturer] = useState("");
+
+  const handleSearch = () => {}
+
   return (
-    <div>SearchBar</div>
+    <form className='searchbar' onSubmit={handleSearch}>
+      <div className="searchbar__item">
+        <SearchManufacturer
+          manufacturer={setManufacturer}
+        />
+      </div>
+    </form>
   )
 }
 
