5bd46b8fcccecc094250f39d3f0eecd05f7805af
jurecapuder <capuder.j@gmail.com>
2023-09-01 00:15:44 +0200

    Fixing spell error

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -20,7 +20,7 @@ const NavBar = () => {
         <CustomButton
           title="Sign In"
           btnType="button"
-          containerStyles="text-primary-blue rounded-full bg.white min-w-[130px]"
+          containerStyles="text-primary-blue rounded-full bg-white min-w-[130px]"
         />
       </nav>
     </header>
