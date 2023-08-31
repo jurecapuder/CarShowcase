b7807c6767da087eac3a8f7d53e061f0dbe93ee9
jurecapuder <capuder.j@gmail.com>
2023-08-31 23:47:48 +0200

    Updating footer

--- components/index.ts
+++ components/index.ts
@@ -1,7 +1,11 @@
 import Hero from './Hero';
 import CustomButton from './CustomButton';
+import NavBar from './NavBar';
+import Footer from './Footer';
 
 export {
   Hero,
-  CustomButton
+  CustomButton,
+  NavBar,
+  Footer
 }
