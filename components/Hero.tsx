4108d0127b6a08885ad7965ecdcacf5322415eaf
jurecapuder <capuder.j@gmail.com>
2023-09-01 00:16:01 +0200

    Fixing container error

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -29,11 +29,9 @@ const Hero = () => {
       <div className="hero__image-container">
         <div className="hero__image">
           <Image src="/hero.png" alt="hero" fill className="object-contain" />
-
-          <div className="hero__image-overlay">
-            
-          </div>
         </div>
+
+        <div className="hero__image-overlay" />
       </div>
     </div>
   )
