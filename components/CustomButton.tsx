42a20cc573291a8b889c1cbfd64a25fc465d1a50
jurecapuder <capuder.j@gmail.com>
2023-08-31 16:02:25 +0200

    Creating CustomButton

---
+++ components/CustomButton.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+
+const CustomButton = () => {
+  return (
+    <button
+      disabled={false}
+      type={"button"}
+      className={`custom-btn`}
+      onClick={() => {}}
+    >
+      
+    </button>
+  )
+}
+
+export default CustomButton
