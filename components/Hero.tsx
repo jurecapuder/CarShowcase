1a7692b33ac2ff179fef8537a3649a1dd8e9095c
jurecapuder <capuder.j@gmail.com>
2023-08-31 16:16:49 +0200

    Updating hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,7 +1,13 @@
+"use client";
+
 import Image from "next/image";
 import CustomButton from "./CustomButton";
 
 const Hero = () => {
+  const handleScroll = () => {
+
+  }
+
   return (
     <div className="hero">
       <div className="flex-1 pt-36 ppadding-x">
@@ -13,7 +19,21 @@ const Hero = () => {
           Streamline your car rental experience with out effortless booking process.
         </p>
 
-        <CustomButton /> 
+        <CustomButton
+          title="Explore Cars"
+          containerStyles="bg-primary-blue text-white rounded-full mt-10"
+          handleClick={handleScroll}
+        />
+      </div>
+
+      <div className="hero__image-container">
+        <div className="hero__image">
+          <Image src="/hero.png" alt="hero" fill className="object-contain" />
+
+          <div className="hero__image-overlay">
+            
+          </div>
+        </div>
       </div>
     </div>
   )
