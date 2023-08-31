8536ad3360b30c7183ae3490ef4e60c07fcc793a
jurecapuder <capuder.j@gmail.com>
2023-08-31 16:02:38 +0200

    Adding Hero to page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Image from 'next/image'
 
+import { Hero } from '@/components'
+
 export default function Home() {
   return (
     <main className='overflow-hidden'>
-      
+      <Hero />
     </main>
   )
 }
