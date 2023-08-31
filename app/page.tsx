0608ef3f6cb65a1edd353b2a82cd9ad17bebe780
jurecapuder <capuder.j@gmail.com>
2023-09-01 00:45:09 +0200

    Adding car catalogue

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,28 @@
 import Image from 'next/image'
 
-import { Hero } from '@/components'
+import { CustomFilter, Hero, SearchBar } from '@/components'
 
 export default function Home() {
   return (
     <main className='overflow-hidden'>
       <Hero />
+      
+      <div className='mt-12 padding-x padding-y max-width' id='discover'>
+        <div className='home__text-container'>
+          <h1 className='text-4xl font-extrabold'>Car Catalogue</h1>
+
+          <p>Explore the cars you might like</p>
+        </div>
+
+        <div className='home__filters'>
+          <SearchBar />
+
+          <div className='home__filter-container'>
+            <CustomFilter title="fuel" />
+            <CustomFilter title="year" />
+          </div>
+        </div>
+      </div>
     </main>
   )
 }
