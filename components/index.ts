ab2abaa6c6f57b704d74789517dfee7882b84dac
jurecapuder <capuder.j@gmail.com>
2023-09-03 23:14:41 +0200

    Adding carcard

--- components/index.ts
+++ components/index.ts
@@ -5,6 +5,7 @@ import Footer from './Footer';
 import SearchBar from './SearchBar';
 import CustomFilter from './CustomFilter';
 import SearchManufacturer from './SearchManufacturer';
+import CarCard from './CarCard';
 
 export {
   Hero,
@@ -13,5 +14,6 @@ export {
   Footer,
   SearchBar,
   CustomFilter,
-  SearchManufacturer
+  SearchManufacturer,
+  CarCard
 }
