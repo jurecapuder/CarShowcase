a4ae60a1940ed2a24838dd8f6425fb2cc5518a12
jurecapuder <capuder.j@gmail.com>
2023-09-03 23:14:50 +0200

    Adding car props

--- types/index.ts
+++ types/index.ts
@@ -11,3 +11,18 @@ export interface SearchManufaturerProps {
   manufacturer: string;
   setManufacturer: (manufacturer: string) => void;
 }
+
+export interface CarProps {
+  city_mpg: number;
+  class: string;
+  combination_mpg: number;
+  cylinders: number;
+  displacement: number;
+  drive: string;
+  fuel_type: string;
+  highway_mpg: number;
+  make: string;
+  model: string;
+  transmission: string;
+  year: number;
+}
