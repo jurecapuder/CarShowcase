400163f135487ca47cb8029a5ff2e80c6c9b7a6e
jurecapuder <capuder.j@gmail.com>
2023-09-04 01:23:35 +0200

    exporting car details

--- components/index.ts
+++ components/index.ts
@@ -6,6 +6,7 @@ import SearchBar from './SearchBar';
 import CustomFilter from './CustomFilter';
 import SearchManufacturer from './SearchManufacturer';
 import CarCard from './CarCard';
+import CarDetails from './CarDetails';
 
 export {
   Hero,
@@ -15,5 +16,6 @@ export {
   SearchBar,
   CustomFilter,
   SearchManufacturer,
-  CarCard
+  CarCard,
+  CarDetails
 }
