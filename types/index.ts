21052aefa9dcb895737c551e92113a92ca66ce5a
jurecapuder <capuder.j@gmail.com>
2023-09-04 01:23:21 +0200

    Adding more props to custom button

--- types/index.ts
+++ types/index.ts
@@ -5,6 +5,9 @@ export interface CustomButtonProps {
   containerStyles?: string;
   handleClick?: MouseEventHandler<HTMLButtonElement>;
   btnType?: "button" | "submit";
+  textStyles?: string;
+  rightIcon?: string;
+  isDisabled?: boolean;
 }
 
 export interface SearchManufaturerProps {
