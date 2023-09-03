5df2dfa565b5d04c47bc23083f3d95205b64e0f0
jurecapuder <capuder.j@gmail.com>
2023-09-03 23:39:13 +0200

    Adding rent price

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -5,6 +5,7 @@ import Image from 'next/image';
 
 import { CarProps } from '@/types';
 import CustomButton from './CustomButton';
+import { calculateCarRent } from '@/utils';
 
 interface CarCardProps {
   car: CarProps;
@@ -13,9 +14,27 @@ interface CarCardProps {
 const CarCard = ({ car }: CarCardProps) => {
   const { city_mpg, year, make, model, transmission, drive } = car;
 
+  const carRent = calculateCarRent(city_mpg, year);
+
   return (
     <div className='car-card group'>
+      <div className='car-card __content'>
+        <h2 className='car-card__content-title'>
+          {make} {model}
+        </h2>
+      </div>
 
+      <p className='flex mt-6 text-[32px] font-extrabold'>
+        <span className='self-start text-[14px] font-semibold'>
+          $
+        </span>
+        
+        {carRent}
+
+        <span className='self-end text-[14px] font-medium'>
+          /day
+        </span>
+      </p>
     </div>
   )
 }
