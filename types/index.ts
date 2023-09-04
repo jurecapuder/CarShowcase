f13c3316ced585c33b59968d05dfe25870d69fa9
jurecapuder <capuder.j@gmail.com>
2023-09-04 03:55:02 +0200

    Adding filter, option and custom filter props

--- types/index.ts
+++ types/index.ts
@@ -29,3 +29,21 @@ export interface CarProps {
   transmission: string;
   year: number;
 }
+
+export interface FilterProps {
+  manufacturer: string;
+  year: number;
+  fuel: string;
+  limit: number;
+  model: string;
+}
+
+export interface OptionProps {
+  title: string;
+  value: string;
+}
+
+export interface CustomFilterProps {
+  title: string;
+  options: OptionProps[];
+}
