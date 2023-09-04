ba3a496b13a255f8f0c72abb6488334a7d5aac60
jurecapuder <capuder.j@gmail.com>
2023-09-04 02:47:33 +0200

    Updating search bar

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -3,11 +3,26 @@
 import { useState } from "react";
 
 import { SearchManufacturer } from "./";
+import Image from "next/image";
+
+const SearchButton = ({ otherClasses }: { otherClasses: string }) => (
+  <button type="submit" className={`-ml-3 z-10 ${otherClasses}`}>
+    <Image
+      src="/magnifying-glass.svg"
+      alt="magnifying glass"
+      width={40}
+      height={40}
+      className="object-contain"
+    />
+  </button>
+)
 
 const SearchBar = () => {
   const [manufacturer, setManufacturer] = useState("");
+  const [model, setModel] = useState("");
 
-  const handleSearch = () => {}
+  const handleSearch = () => {
+  }
 
   return (
     <form className='searchbar' onSubmit={handleSearch}>
@@ -15,9 +30,33 @@ const SearchBar = () => {
         <SearchManufacturer
           manufacturer={manufacturer}
           setManufacturer={setManufacturer}
-
         />
+
+        <SearchButton otherClasses="sm:hidden" />
       </div>
+
+      <div className="searchbar__item">
+        <Image
+          src="/model-icon.png"
+          width={25}
+          height={25}
+          className="absolute w-[20px] h-[20px] ml-4"
+          alt="car model"
+        />
+
+        <input
+          type="text"
+          name="model"
+          value={model}
+          onChange={(e) => setModel(e.target.value)}
+          placeholder="Tiguan"
+          className="searchbar__input"
+        />
+
+        <SearchButton otherClasses="sm:hidden" />
+      </div>
+
+      <SearchButton otherClasses="max-sm:hidden" />
     </form>
   )
 }
