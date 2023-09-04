39d4c1092d2ee75f4ec044d80e0e76b673a3202b
jurecapuder <capuder.j@gmail.com>
2023-09-04 04:14:49 +0200

    Importing show more

--- components/index.ts
+++ components/index.ts
@@ -7,6 +7,7 @@ import CustomFilter from './CustomFilter';
 import SearchManufacturer from './SearchManufacturer';
 import CarCard from './CarCard';
 import CarDetails from './CarDetails';
+import ShowMore from './ShowMore';
 
 export {
   Hero,
@@ -17,5 +18,6 @@ export {
   CustomFilter,
   SearchManufacturer,
   CarCard,
-  CarDetails
+  CarDetails,
+  ShowMore
 }
