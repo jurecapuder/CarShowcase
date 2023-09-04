1b2e188f038276db1f0560ee6c39618cb8f9e508
jurecapuder <capuder.j@gmail.com>
2023-09-04 02:05:45 +0200

    Updating car images

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -5,6 +5,7 @@ import Image from 'next/image';
 import { Dialog, Transition } from '@headlessui/react';
 
 import { CarProps } from '@/types';
+import { generateCarImageUrl } from '@/utils';
 
 interface CarDetailsProps {
   isOpen: boolean,
@@ -68,7 +69,7 @@ const CarDetails = ({ isOpen, closeModal, car}: CarDetailsProps) => {
               <div className='flex-1 flex flex-col gap-3'>
                 <div className='relative w-full h-40 bg-pattern bg-cover bg-center rounded-lg'>
                 <Image
-                  src="/hero.png"
+                  src={generateCarImageUrl(car)}
                   alt="Car Model"
                   fill
                   priority
@@ -79,7 +80,7 @@ const CarDetails = ({ isOpen, closeModal, car}: CarDetailsProps) => {
                 <div className='flex gap-3'>
                   <div className='flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg'>
                   <Image
-                    src="/hero.png"
+                    src={generateCarImageUrl(car, "29")}
                     alt="Car Model"
                     fill
                     priority
@@ -89,7 +90,7 @@ const CarDetails = ({ isOpen, closeModal, car}: CarDetailsProps) => {
 
                   <div className='flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg'>
                   <Image
-                    src="/hero.png"
+                    src={generateCarImageUrl(car, "33")}
                     alt="Car Model"
                     fill
                     priority
@@ -99,7 +100,7 @@ const CarDetails = ({ isOpen, closeModal, car}: CarDetailsProps) => {
 
                   <div className='flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg'>
                   <Image
-                    src="/hero.png"
+                    src={generateCarImageUrl(car, "13")}
                     alt="Car Model"
                     fill
                     priority
