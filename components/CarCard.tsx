5a074b7ec53800360ae0ab0f85e5fc0f2f4019f3
jurecapuder <capuder.j@gmail.com>
2023-09-04 02:05:21 +0200

    Updating car card

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -5,7 +5,7 @@ import Image from 'next/image';
 
 import { CarProps } from '@/types';
 import CustomButton from './CustomButton';
-import { calculateCarRent } from '@/utils';
+import { calculateCarRent, generateCarImageUrl } from '@/utils';
 import { CarDetails } from '.';
 
 interface CarCardProps {
@@ -41,7 +41,7 @@ const CarCard = ({ car }: CarCardProps) => {
 
       <div className='relative w-full h-40 my-3 object-contain'>
         <Image
-          src="/hero.png"
+          src={generateCarImageUrl(car)}
           alt="Car Model"
           fill
           priority
