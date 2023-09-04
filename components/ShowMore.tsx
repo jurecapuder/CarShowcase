1a42f904d61a799da1c796aee1e7fecc815ed56d
jurecapuder <capuder.j@gmail.com>
2023-09-04 04:15:15 +0200

    Creating show more component

---
+++ components/ShowMore.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { ShowMoreProps } from "@/types";
+import { useRouter } from "next/navigation";
+import { CustomButton } from ".";
+import { updateSearchParams } from "@/utils";
+
+const ShowMore = ({ pageNumber, isNext }: ShowMoreProps) => {
+  const router = useRouter();
+
+  const handleNavigation = () => {
+    const newLimit = (pageNumber + 1) * 10;
+
+    const newPathName = updateSearchParams("limit", `${newLimit}`);
+
+    router.push(newPathName, {scroll: false});
+  }
+
+  return (
+    <div className="w-full flex-center gap-5 mt-10">
+      {!isNext && (
+        <CustomButton
+          title="Show More"
+          btnType="button"
+          containerStyles="bg-primary-blue rounded-full text-white"
+          handleClick={handleNavigation}
+        />
+      )}
+    </div>
+  )
+}
+
+export default ShowMore
