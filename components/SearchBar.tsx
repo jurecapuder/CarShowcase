9f408b4944c137e6fd90cf834302f9f6eb880d4b
jurecapuder <capuder.j@gmail.com>
2023-09-04 03:09:06 +0200

    Updating search bar

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,9 +1,10 @@
 "use client";
 
-import { useState } from "react";
+import React, { useState } from "react";
 
 import { SearchManufacturer } from "./";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 const SearchButton = ({ otherClasses }: { otherClasses: string }) => (
   <button type="submit" className={`-ml-3 z-10 ${otherClasses}`}>
@@ -20,8 +21,39 @@ const SearchButton = ({ otherClasses }: { otherClasses: string }) => (
 const SearchBar = () => {
   const [manufacturer, setManufacturer] = useState("");
   const [model, setModel] = useState("");
+  const router = useRouter();
 
-  const handleSearch = () => {
+  const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if(manufacturer === "" && model === "") {
+      return alert("Please fill in the search bar.")
+    }
+
+    updateSeatchParams(
+      model.toLowerCase(),
+      manufacturer.toLowerCase()
+    )
+  }
+
+  const updateSeatchParams = (model: string, manufacturer: string) => {
+    const searchParams = new URLSearchParams(window.location.search);
+
+    if (model) {
+      searchParams.set("model", model)
+    } else {
+      searchParams.delete("model")
+    }
+
+    if (manufacturer) {
+      searchParams.set("manufacturer", manufacturer)
+    } else {
+      searchParams.delete("manufacturer")
+    }
+
+    const newPathName = `${window.location.pathname} ? ${searchParams.toString()}`
+
+    router.push(newPathName)
   }
 
   return (
