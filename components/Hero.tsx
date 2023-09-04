56594edc8da53eea0d4c1bb6179394b28ec57dd2
jurecapuder <capuder.j@gmail.com>
2023-09-04 04:14:36 +0200

    Fixing spelling error

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -10,7 +10,7 @@ const Hero = () => {
 
   return (
     <div className="hero">
-      <div className="flex-1 pt-36 ppadding-x">
+      <div className="flex-1 pt-36 padding-x">
         <h1 className="hero__title">
           Find, book or rent a car — quickly and easily!
         </h1>
