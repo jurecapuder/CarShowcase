f5ac47104e21b0fd055ab738a66a0ca5bc42762d
jurecapuder <capuder.j@gmail.com>
2023-09-06 02:08:00 +0200

    Updating explore cars button

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -5,7 +5,11 @@ import CustomButton from "./CustomButton";
 
 const Hero = () => {
   const handleScroll = () => {
+    const nextSection = document.getElementById("discover");
 
+    if (nextSection) {
+      nextSection.scrollIntoView({ behavior: "smooth" });
+    }
   }
 
   return (
